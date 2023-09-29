242fb29ff342ca5b3b21a66649c7ea544dec27a1
Mehsheed Ahmed Syed Abdul <mehsheed59@gmail.com>
2023-09-29 01:11:25 -0600

    fix : Handle 301 properly, docs: root.png,deep.png

--- server.py
+++ server.py
@@ -43,7 +43,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
                                     <h1>404 - Page Not Found</h1>
                                     <p>The page you are looking for does not exist.</p>
                                 </body>
-                                </html> """
+                                </html> \n"""
             body_len = str(len(response_body))
             return response_body,body_len
         elif status_code == 405:
@@ -56,9 +56,10 @@ class MyWebServer(socketserver.BaseRequestHandler):
                                     <h1>405 - Method Not Allowed</h1>
                                     <p>The method you are using is not allowed.</p>
                                 </body>
-                                </html> """
+                                </html> \n"""
             body_len = str(len(response_body))
             return response_body,body_len
+        
 
 
 
@@ -78,13 +79,6 @@ class MyWebServer(socketserver.BaseRequestHandler):
             response = "HTTP/1.1 200 OK\r\n" + "Content-Type: " + file_type + "\r\n" + "Content-Length: " + str(len(response_body)) + "\r\n" + "Connection: close\r\n\r\n" + response_body
             f.close()
             return response
-        elif status_code == 301:
-            f = open(file_path + "index.html",'r')
-            response_body = f.read()
-            response = "HTTP/1.1 301 Moved Permanently\r\n" + "Content-Type: " + file_type + "\r\n" + "Content-Length: " + str(len(response_body)) + "\r\n" +"Connection: close\r\n" + "Location: " +file_path+"/"+ "\r\n\r\n" + response_body
-            f.close()
-            
-            return response
         elif status_code == 404:
             response_body,content_len = self.create_html_template_error(status_code)
             response = "HTTP/1.1 404 Not Found\r\n" + "Content-Type: " + "text/html" + "\r\n" +"Referrer-Policy: "+"no-referrer\r\n" +"Content-Length: " + content_len + "\r\n" +"Connection: close" + "\r\n\r\n" + response_body
@@ -93,9 +87,24 @@ class MyWebServer(socketserver.BaseRequestHandler):
             response_body,content_len = self.create_html_template_error(status_code)
             response = "HTTP/1.1 405 Method Not Allowed\r\n" + "Allow: GET\r\n" +""+"Content-Type: " + "text/html" + "Content-Length: " + content_len+ "\r\n" +"Connection: close" + "\r\n\r\n" + response_body
             return response
+        # elif status_code == 301:
+        #     f = open(file_path + "index.html",'r')
+        #     response_body = f.read()
+        #     response = "HTTP/1.1 301 Moved Permanently\r\n" + "Content-Type: " + file_type + "\r\n" + "Content-Length: " + str(len(response_body)) + "\r\n" +"Connection: close\r\n" + "Location: " +file_path+"/"+ "\r\n\r\n" + response_body
+        #     f.close()
+
+
         
     
+    def moved_permanently_response(self, redirect_path, file_type):
+        f = open(redirect_path+"index.html", 'r')
+        response_body = f.read()
+        response = "HTTP/1.1 301 Moved Permanently\r\n" + "\r\n"+"Location: "+redirect_path+"index.html"+"\r\n"+"Content-Type: " + file_type + "\r\n" + "Content-Length: " + str(len(response_body)) + "\r\n" + \
+        "Connection: close\r\n\r\n" + response_body
+        f.close()
+        return response
         
+            
             
     def check_file_type(self, file_path):
 
@@ -177,7 +186,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
                     redirect_path = path+"/"
                     print(redirect_path)
                     #Create 301 Moved Permanently response and redirect
-                    response = self.create_response(redirect_path, file_type,301)
+                    response = self.moved_permanently_response(redirect_path, file_type)
                     self.request.sendall(bytearray(response,'utf-8'))
             #Handle directory traversal attempts and invalid paths
             else:
