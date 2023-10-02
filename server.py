e62f36bcda840fb93d01c18e24aacb387bcae003
Mehsheed Ahmed Syed Abdul <mehsheed59@gmail.com>
2023-10-02 01:32:04 -0600

    fix: 301,format

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
 import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,10 +29,7 @@ import os
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-
-    
-    #TODO : Create a HTML_Template function that takes Response Code and generates standard HTML response for response function
-    def create_html_template_error(self,status_code):
+    def create_html_template_error(self, status_code):
         if status_code == 404:
             response_body = """<!DOCTYPE html>
                                 <html>
@@ -45,7 +42,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
                                 </body>
                                 </html> \n"""
             body_len = str(len(response_body))
-            return response_body,body_len
+            return response_body, body_len
         elif status_code == 405:
             response_body = """<!DOCTYPE html>
                                 <html>
@@ -58,57 +55,110 @@ class MyWebServer(socketserver.BaseRequestHandler):
                                 </body>
                                 </html> \n"""
             body_len = str(len(response_body))
-            return response_body,body_len
-        
-
-
-
+            return response_body, body_len
 
     def create_response(self, file_path, file_type, status_code):
-
-        #Create a response header
+        # Create a response header
         response_header = ""
         response_body = ""
         response = ""
-        #Create a response body
+        # Create a response body
         if status_code == 200:
             f = open(file_path, "r")
-            response_body  = f.read()
-            
-            #genreate response header
-            response = "HTTP/1.1 200 OK\r\n" + "Content-Type: " + file_type + "\r\n" + "Content-Length: " + str(len(response_body)) + "\r\n" + "Connection: close\r\n\r\n" + response_body
+            response_body = f.read()
+
+            # genreate response header
+            response = (
+                "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: "
+                + file_type
+                + "\r\n"
+                + "Content-Length: "
+                + str(len(response_body))
+                + "\r\n"
+                + "Connection: close\r\n\r\n"
+                + response_body
+            )
             f.close()
             return response
         elif status_code == 404:
-            response_body,content_len = self.create_html_template_error(status_code)
-            response = "HTTP/1.1 404 Not Found\r\n" + "Content-Type: " + "text/html" + "\r\n" +"Referrer-Policy: "+"no-referrer\r\n" +"Content-Length: " + content_len + "\r\n" +"Connection: close" + "\r\n\r\n" + response_body
+            response_body, content_len = self.create_html_template_error(status_code)
+            response = (
+                "HTTP/1.1 404 Not Found\r\n"
+                + "Content-Type: "
+                + "text/html"
+                + "\r\n"
+                + "Referrer-Policy: "
+                + "no-referrer\r\n"
+                + "Content-Length: "
+                + content_len
+                + "\r\n"
+                + "Connection: close"
+                + "\r\n\r\n"
+                + response_body
+            )
             return response
         elif status_code == 405:
-            response_body,content_len = self.create_html_template_error(status_code)
-            response = "HTTP/1.1 405 Method Not Allowed\r\n" + "Allow: GET\r\n" +""+"Content-Type: " + "text/html" + "Content-Length: " + content_len+ "\r\n" +"Connection: close" + "\r\n\r\n" + response_body
+            response_body, content_len = self.create_html_template_error(status_code)
+            response = (
+                "HTTP/1.1 405 Method Not Allowed\r\n"
+                + "Allow: GET\r\n"
+                + ""
+                + "Content-Type: "
+                + "text/html"
+                + "Content-Length: "
+                + content_len
+                + "\r\n"
+                + "Connection: close"
+                + "\r\n\r\n"
+                + response_body
+            )
             return response
         elif status_code == 301:
-            f = open(file_path + "index.html",'r')
+            f = open(file_path + "index.html", "r")
             response_body = f.read()
-            response = "HTTP/1.1 301 Moved Permanently\r\n" + "Content-Type: " + file_type + "\r\n" + "Content-Length: " + str(len(response_body)) + "\r\n" +"Connection: keep-alive\r\n" + "Location: " +file_path+ "\r\n\r\n" + response_body
+            response = (
+                "HTTP/1.1 301 Moved Permanently\r\n"
+                + "\r\n"
+                + "Location: "
+                + file_path
+                + "index.html"
+                + "\r\n"
+                + "Content-Type: "
+                + file_type
+                + "\r\n"
+                + "Content-Length: "
+                + str(len(response_body))
+                + "\r\n"
+                + "Connection: keep-alive\r\n\r\n"
+                + response_body
+            )
             f.close()
             return response
 
+    def moved_permanently_response(self, redirect_path, file_type):
+        f = open(redirect_path + "index.html", "r")
+        response_body = f.read()
+        response = (
+            "HTTP/1.1 301 Moved Permanently\r\n"
+            + "\r\n"
+            + "Location: "
+            + redirect_path
+            + "index.html"
+            + "\r\n"
+            + "Content-Type: "
+            + file_type
+            + "\r\n"
+            + "Content-Length: "
+            + str(len(response_body))
+            + "\r\n"
+            + "Connection: keep-alive\r\n\r\n"
+            + response_body
+        )
+        f.close()
+        return response
 
-        
-    
-    # def moved_permanently_response(self, redirect_path, file_type):
-    #     f = open(redirect_path+"index.html", 'r')
-    #     response_body = f.read()
-    #     response = "HTTP/1.1 301 Moved Permanently\r\n" + "\r\n"+"Location: "+redirect_path+"index.html"+"\r\n"+"Content-Type: " + file_type + "\r\n" + "Content-Length: " + str(len(response_body)) + "\r\n" + \
-    #     "Connection: keep-alive\r\n\r\n" + response_body
-    #     f.close()
-    #     return response
-        
-            
-            
     def check_file_type(self, file_path):
-
         if ".html" in file_path:
             return "text/html"
         elif ".css" in file_path:
@@ -116,103 +166,73 @@ class MyWebServer(socketserver.BaseRequestHandler):
         else:
             return None
 
-            
-
-
-
-
-
-
-
-
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
+        print("Got a request of: %s\n" % self.data)
         # self.request.sendall(bytearray("OK",'utf-8'))
         decoded_data = self.data.decode("utf-8")
+
         split_data = decoded_data.split()
+
         request_method = split_data[0]
         request_path = split_data[1]
         prefix = "www"
         file_type = ""
 
-        #Update file type for header
-        if ".html" in request_path :
-
+        # Update file type for header
+        if ".html" in request_path:
             file_type = "text/html"
         elif ".css" in request_path:
-                
             file_type = "text/css"
 
-        #Check if request is a GET request
+        # Check if request is a GET request
         if request_method == "GET":
-            
-            
-            path = prefix+request_path
+            path = prefix + request_path
             directory_traversal = "/../" in request_path
-            #Validate path
+            # Validate path
             start_char = request_path[0]
             end_char = request_path[-1]
             valid_path = start_char == "/" and end_char == "/"
 
             if valid_path and not directory_traversal:
-
-                #Check if path is a directory
+                # Check if path is a directory
                 if os.path.isdir(path):
-                    #Create file path to serve the 'index.html' file within the specified address
+                    # Create file path to serve the 'index.html' file within the specified address
                     file_path = prefix + request_path + "index.html"
-                    #Check if the 'file_path' reffered "in"file exists
+                    # Check if the 'file_path' reffered "in"file exists
                     if os.path.isfile(file_path):
-                        #Create 200 OK response
-                        response = self.create_response(file_path,"text/html",200)
-                        #Send response
-                        self.request.sendall(bytearray(response,'utf-8'))
-                #Path is a file
+                        # Create 200 OK response
+                        response = self.create_response(file_path, "text/html", 200)
+                        # Send response
+                        self.request.sendall(bytearray(response, "utf-8"))
+                # Path is a file
                 else:
                     file_path = prefix + "/index.html"
-                    response = self.create_response(file_path,"text/html",200)
-                    self.request.sendall(bytearray(response,'utf-8'))
-
+                    response = self.create_response(file_path, "text/html", 200)
+                    self.request.sendall(bytearray(response, "utf-8"))
 
             elif os.path.exists(path) and not directory_traversal:
-
                 if os.path.isfile(path):
-                    #Serve the file at "path" with the specified 'file_type'
-                    response = self.create_response(path, file_type,200)
-                    self.request.sendall(bytearray(response,'utf-8'))   
+                    # Serve the file at "path" with the specified 'file_type'
+                    response = self.create_response(path, file_type, 200)
+                    self.request.sendall(bytearray(response, "utf-8"))
 
-                elif os.path.isdir(path+"/"):
-                    
+                elif os.path.isdir(path + "/"):
+                    redirect_path = path + "/"
 
-                    redirect_path = path+"/"
-                    print(redirect_path)
-                    #Create 301 Moved Permanently response and redirect
+                    # Create 301 Moved Permanently response and redirect
+
                     # response = self.moved_permanently_response(redirect_path, file_type)
-                    response = self.create_response(redirect_path, file_type,301)
-                    self.request.sendall(bytearray(response,'utf-8'))
-            #Handle directory traversal attempts and invalid paths
+                    response = self.create_response(redirect_path, file_type, 301)
+                    self.request.sendall(bytearray(response, "utf-8"))
+            # Handle directory traversal attempts and invalid paths
             else:
-
-                response = self.create_response(None, file_type,404)
-                self.request.sendall(bytearray(response,'utf-8'))
-        #Handle non-GET requests
+                response = self.create_response(None, file_type, 404)
+                self.request.sendall(bytearray(response, "utf-8"))
+        # Handle non-GET requests
         else:
-            response = self.create_response(None, None,405)
-            self.request.sendall(bytearray(response,'utf-8')) 
-
-
-
-
-
-
-
-
-                
-        
-
-
-
-
+            response = self.create_response(None, None, 405)
+            self.request.sendall(bytearray(response, "utf-8"))
 
 
 if __name__ == "__main__":
@@ -221,7 +241,6 @@ if __name__ == "__main__":
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
-    
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
